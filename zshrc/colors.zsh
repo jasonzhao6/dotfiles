f2081b1411c86139bba13b4990f56737947d9a6d
Jason Zhao <jasonzhao6@gmail.com>
2025-01-02 20:31:00 -0800

    Revise comments in `colors.zsh`

--- zshrc/colors.zsh
+++ zshrc/colors.zsh
@@ -1,5 +1,13 @@
 #
-# Color modes
+# Config
+#
+
+export GREP_COLOR='1;32'
+export LSCOLORS='gxcxbxexfxegedabagaced'
+export JQ_COLORS='1;35:1;35:1;35:1;35:1;32:1;33:1;33:1;36' # v1.7+
+
+#
+# Modes
 #
 
 function color {
@@ -9,7 +17,7 @@ function color {
     alias ls='ls --color=always'
 };
 
-function bw { # black and white
+function bw { # Black and white
     unalias diff
     unalias egrep
     unalias grep
@@ -17,27 +25,19 @@ function bw { # black and white
 }
 
 #
-# Color configs
+# Setters
 #
 
-export GREP_COLOR='1;32'
-export LSCOLORS='gxcxbxexfxegedabagaced'
-export JQ_COLORS='1;35:1;35:1;35:1;35:1;32:1;33:1;33:1;36' # v1.7+
-
-#
-# Color setters
-#
-
-# set foreground
+# Set foreground
 function cyan-fg { echo "\e[36m$*\e[0m"; }
 function gray-fg { echo "\e[90m$*\e[0m"; }
 
-# set foreground by use case
+# Set foreground by use case
 BACKTICK="\e[90m\`\e[0m"
 function command-color { echo "$BACKTICK$(cyan-fg "$@")$BACKTICK"; }
 function command-color-dim { echo "$BACKTICK$(gray-fg "$@")$BACKTICK"; }
 function grep-color { echo "\e[1;32m\e[K$*\e[m\e[K"; }
 
-# set background
+# Set background
 function red-bg { echo "\e[41m$*\e[0m"; }
 function green-bg { echo "\e[42m$*\e[0m"; }
