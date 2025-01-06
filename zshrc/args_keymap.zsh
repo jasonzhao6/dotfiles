6704f08dededd8f46844dae02a59c058f6c76678
Jason Zhao <jasonzhao6@gmail.com>
2025-01-05 23:50:23 -0800

    Teach `args_keymap` to handle `|`

--- zshrc/args_keymap.zsh
+++ zshrc/args_keymap.zsh
@@ -13,45 +13,51 @@ ARGS_KEYMAP=(
 	'a h'
 )
 
+ARGS_SOFT_SELECT='Soft-select the 1st column by inserting a `#` before the 2nd column'
+
 function a {
-	local namespace='a'
-	keymap $namespace ${#ARGS_KEYMAP} "${ARGS_KEYMAP[@]}" "$@"
-	echo exit code: $?
+	if [[ -t 0 ]]; then
+		# When not invoked after a `|`
+		local namespace='a'
+		keymap $namespace ${#ARGS_KEYMAP} "${ARGS_KEYMAP[@]}" "$@"
+	else
+		# When invoked after a `|`
+		local key=$1
+		[[ $key == so ]] && args_save "$ARGS_SOFT_SELECT" || args_save
+	fi
 }
 
 #
 # Key mappings
 #
 
+# TODO
 function a_a {
 	if [[ -z $1 ]]; then
 		echo if
 		args_list
 	else
 		echo else
-		args_plain | eval "$(args_filtering "$@") | $(args_coloring "$@")" | a s
+		args_plain | eval "$(args_filtering "$@") | $(args_coloring "$@")" | a_s
 	fi
 }
 
+# TODO
 function a_h {
 	if [[ -z $1 || $1 -lt $ARGS_TAIL || $1 -gt $ARGS_HEAD ]]; then
 		args_history
 	else
+		# shellcheck disable=SC2034
 		ARGS_CURSOR=$1
-		a a
+		a_a
 	fi
 }
 
 function a_s {
-	if [[ -t 0 ]]; then
-		# Save the output of the previous command
-		eval "$(prev_command)" | args_save "$@"
-	else
-		# Save the output of the upstream `|` command
-		args_save "$@"
-	fi
+	local is_soft_select=$1
+	eval "$(prev_command)" | args_save "$is_soft_select"
 }
 
-function a_so() {
-	a s 'Soft-select the 1st column by inserting a `#` before the 2nd column'
+function a_so {
+	a_s "$ARGS_SOFT_SELECT"
 }
