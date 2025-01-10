b5476181cc799de8827b1f08697d49f4fc24611e
Jason Zhao <jasonzhao6@gmail.com>
2025-01-09 22:11:52 -0800

    Update `git_keymap` to latest version

--- zshrc/git_keymap.zsh
+++ zshrc/git_keymap.zsh
@@ -1,47 +1,47 @@
-#
-# Namespace: [G]it # TODO
-# Alias: Git[H]ub
-#
+GIT_NAMESPACE='git_keymap'
+GIT_ALIAS='h'
 
 GIT_KEYMAP=(
-	'h·on # Create a new PR, then open tab to it'
-	'h·og # Open tab to create a new gist'
+	"$GIT_ALIAS·on # Create a new PR, then open tab to it"
+	"$GIT_ALIAS·og # Open tab to create a new gist"
 	''
-	'h·oc # Open tab to the latest commit'
-	'h·oc <sha> # Open tab to the specified commit'
-	'h·op # Open tab to the latest PRs'
-	'h·op <pr> # Open tab to the specified PR'
-	'h·or # Open tab to the current repo'
-	'h·or <repo> # Open tab to the specified repo'
+	"$GIT_ALIAS·oc # Open tab to the latest commit"
+	"$GIT_ALIAS·oc <sha> # Open tab to the specified commit"
+	"$GIT_ALIAS·op # Open tab to the latest PRs"
+	"$GIT_ALIAS·op <pr> # Open tab to the specified PR"
+	"$GIT_ALIAS·or # Open tab to the current repo"
+	"$GIT_ALIAS·or <repo> # Open tab to the specified repo"
 )
 
-function h {
-	keymap h ${#GIT_KEYMAP} "${GIT_KEYMAP[@]}" "$@"
+keymap_init $GIT_NAMESPACE $GIT_ALIAS "${GIT_KEYMAP[@]}"
+
+function git_keymap {
+	keymap_invoke $GIT_NAMESPACE $GIT_ALIAS ${#GIT_KEYMAP} "${GIT_KEYMAP[@]}" "$@"
 }
 
 #
 # Key mappings (Alphabetized)
 #
 
-function hoc {
+function git_keymap_oc {
 	open https://"$(domain)"/"$(org)"/"$(repo)"/commit/"$1"
 }
 
 # To be overwritten by `$ZSH_SECRETS`
-GIST='https://gist.github.com'
+GIST='https://gist.git_keymaphub.com'
 
-function hog {
+function git_keymap_og {
 	open $GIST
 }
 
-function hon {
+function git_keymap_on {
 	gp && gh pr create --fill && gh pr view --web
 }
 
-function hop {
+function git_keymap_op {
 	open https://"$(domain)"/"$(org)"/"$(repo)"/pull/"$1"
 }
 
-function hor {
+function git_keymap_or {
 	open https://"$(domain)"/"$(org)"/"${*:-$(repo)}"
 }
