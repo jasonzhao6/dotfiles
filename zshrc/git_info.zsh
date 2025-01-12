b4380daf9ec8ae4e193d510aa66ce524b9bdc990
Jason Zhao <jasonzhao6@gmail.com>
2025-01-12 01:42:21 -0800

    Move `git_info` into `git_keymap`

--- zshrc/git_info.zsh
+++
@@ -1,5 +0,0 @@
-### Git info
-function domain { git remote get-url origin | sed 's/.*[:/]\([^/]*\)\/.*\/.*/\1/'; }
-function org { git remote get-url origin | sed 's/.*[:/]\([^/]*\)\/.*/\1/'; }
-function repo { git rev-parse --show-toplevel | xargs basename; }
-function branch { git rev-parse --abbrev-ref HEAD; }
--- zshrc/git_keymap.zsh
+++ zshrc/git_keymap.zsh
@@ -11,6 +11,11 @@ GIT_KEYMAP=(
 	"$GIT_ALIAS·op <pr> # Open tab to the specified PR"
 	"$GIT_ALIAS·or # Open tab to the current repo"
 	"$GIT_ALIAS·or <repo> # Open tab to the specified repo"
+	''
+	"$GIT_ALIAS·nd # Git domain name"
+	"$GIT_ALIAS·no # Git org name"
+	"$GIT_ALIAS·nr # Git repo name"
+	"$GIT_ALIAS·nb # Git branch name"
 )
 
 keymap_init $GIT_NAMESPACE $GIT_ALIAS "${GIT_KEYMAP[@]}"
@@ -23,22 +28,38 @@ function git_keymap {
 # Key mappings (Alphabetized)
 #
 
-function git_keymap_oc {
-	open https://"$(domain)"/"$(org)"/"$(repo)"/commit/"$1"
+function git_keymap_nb {
+	git rev-parse --abbrev-ref HEAD
 }
 
-function git_keymap_op {
-	open https://"$(domain)"/"$(org)"/"$(repo)"/pull/"$1"
-}
-
-function git_keymap_or {
-	open https://"$(domain)"/"$(org)"/"${*:-$(repo)}"
+function git_keymap_nd {
+	git remote get-url origin | sed 's/.*[:/]\([^/]*\)\/.*\/.*/\1/'
 }
 
 function git_keymap_ng {
 	pbpaste | gh gist create --web
 }
 
+function git_keymap_no {
+	git remote get-url origin | sed 's/.*[:/]\([^/]*\)\/.*/\1/'
+}
+
 function git_keymap_np {
 	gp && gh pr create --fill && gh pr view --web
+}
+
+function git_keymap_nr {
+	git rev-parse --show-toplevel | xargs basename
+}
+
+function git_keymap_oc {
+	open https://"$(git_keymap_nd)"/"$(git_keymap_no)"/"$(git_keymap_nr)"/commit/"$1"
+}
+
+function git_keymap_op {
+	open https://"$(git_keymap_nd)"/"$(git_keymap_no)"/"$(git_keymap_nr)"/pull/"$1"
+}
+
+function git_keymap_or {
+	open https://"$(git_keymap_nd)"/"$(git_keymap_no)"/"${*:-$(git_keymap_nr)}"
 }
