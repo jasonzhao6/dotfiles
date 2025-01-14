801fef7d40f78fc2e642de2f9e3dedcf57db0b16
Jason Zhao <jasonzhao6@gmail.com>
2025-01-13 19:45:30 -0800

    Add todos of <namespace> functions to overwrite

--- zshrc/main.zsh
+++ zshrc/main.zsh
@@ -1,10 +1,10 @@
 # When profiling, set to 1; otherwise, set to <empty>
 ZSHRC_UNDER_PROFILING=
 
-# Profile loading time: Start
+# Profile the load time: Start
 [[ -z $ZSHRC_UNDER_TESTING && -n $ZSHRC_UNDER_PROFILING ]] && zmodload zsh/zprof
 
-# Track loading time: Start
+# Track the load time: Start
 [[ -z $ZSHRC_UNDER_TESTING ]] && ZSHRC_LOAD_TIME_START=$(gdate +%s.%2N)
 
 export DOTFILES_DIR="$HOME/gh/jasonzhao6/dotfiles"
@@ -20,14 +20,14 @@ source "$ZSHRC_DIR/_keymap.zsh"
 source "$ZSHRC_DIR/_reserved_keywords.zsh"
 
 # Load keymaps
-source "$ZSHRC_DIR/args_keymap.zsh"
+source "$ZSHRC_DIR/args_keymap.zsh" # Overwrote `<namespace>` to invoke `args_keymap_a` TODO | a; if not key, filter
 source "$ZSHRC_DIR/aws_keymap.zsh"
-source "$ZSHRC_DIR/git_keymap.zsh"
-source "$ZSHRC_DIR/github_keymap.zsh"
+source "$ZSHRC_DIR/git_keymap.zsh" # Overwrote `<namespace>` to invoke `git_keymap_c` TODO g -> gc
+source "$ZSHRC_DIR/github_keymap.zsh" # Overwrote `<namespace>` to invoke `github_keymap_h` TODO h -> hh
 source "$ZSHRC_DIR/kubectl_keymap.zsh"
 source "$ZSHRC_DIR/main_keymap.zsh"
-source "$ZSHRC_DIR/nav_keymap.zsh" # Has extended `<namespace>` function
-source "$ZSHRC_DIR/other_keymap.zsh" # Has extended `<namespace>` function
+source "$ZSHRC_DIR/nav_keymap.zsh" # Overwrote `<namespace>` to invoke `nav_keymap_n`
+source "$ZSHRC_DIR/other_keymap.zsh" # Overwrote `<namespace>` to invoke `other_keymap_o`
 source "$ZSHRC_DIR/terraform_keymap.zsh"
 source "$ZSHRC_DIR/zsh_keymap.zsh"
 
@@ -36,11 +36,11 @@ source "$ZSHRC_DIR/zsh_arrow_keys.zsh"
 source "$ZSHRC_DIR/zsh_history.zsh"
 source "$ZSHRC_DIR/zsh_prompt.zsh"
 
-# Overwrite placeholders with secret values from `ZSHRC_SECRETS`
+# Source secrets
 [[ -z $ZSHRC_UNDER_TESTING && -f $ZSHRC_SECRETS ]] && source "$ZSHRC_SECRETS"
 
-# Track loading time: Finish
+# Track the load time: Finish
 [[ -z $ZSHRC_UNDER_TESTING ]] && gray_fg "\n\`.zshrc\` loaded in $(echo "$(gdate +%s.%2N) - $ZSHRC_LOAD_TIME_START" | bc) seconds"
 
-# Profile loading time: Finish
+# Profile the load time: Finish
 [[ -z $ZSHRC_UNDER_TESTING && -n $ZSHRC_UNDER_PROFILING ]] && echo && zprof
