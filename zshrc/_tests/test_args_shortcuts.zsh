ae88f94e438afaabf6f6cecc6be052359a2eaacb
Jason Zhao <jasonzhao6@gmail.com>
2025-01-13 20:39:16 -0800

    Add args shortcuts 21 to 100

--- zshrc/_tests/test_args_shortcuts.zsh
+++ zshrc/_tests/test_args_shortcuts.zsh
@@ -1,3 +1,15 @@
+function test__0 {
+	assert "$(
+		echo "$test__input" | args_keymap_s > /dev/null
+		0 echo 2>&1
+	)" "$(
+		cat <<-eof
+			echo terraform-application-region-program-B
+			terraform-application-region-program-B
+		eof
+	)"
+}; run_with_filter test__0
+
 function test__1 {
 	assert "$(
 		echo "$test__input" | args_keymap_s > /dev/null
@@ -28,15 +40,3 @@ function test__6 {
 		6 echo 2>&1
 	)" "echo "
 }; run_with_filter test__6
-
-function test__0 {
-	assert "$(
-		echo "$test__input" | args_keymap_s > /dev/null
-		0 echo 2>&1
-	)" "$(
-		cat <<-eof
-			echo terraform-application-region-program-B
-			terraform-application-region-program-B
-		eof
-	)"
-}; run_with_filter test__0
