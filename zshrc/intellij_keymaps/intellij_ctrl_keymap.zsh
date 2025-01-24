04a39fca0835bb767c045a9cb99fa51d421996e1
Jason Zhao <jasonzhao6@gmail.com>
2025-01-23 20:32:28 -0800

    Add IntelliJ `ctrl` keyboard shortcuts

--- zshrc/intellij_keymaps/intellij_ctrl_keymap.zsh
+++ zshrc/intellij_keymaps/intellij_ctrl_keymap.zsh
@@ -6,8 +6,39 @@ INTELLIJ_CTRL_NAMESPACE='intellij_ctrl_keymap'
 INTELLIJ_CTRL_ALIAS='ic'
 
 INTELLIJ_CTRL_KEYMAP=(
-	"ctrl-c # Copy"
-	"ctrl-v # Paste"
+	"ctrl-- # Copilot show chat"
+	"ctrl-shift-- # Copilot new conversation"
+	"ctrl-= # Copilot show completions"
+	"ctrl-shift-= # IntelliJ code generate"
+	"ctrl-$KEYMAP_ESCAPE # Copilot enable completions"
+	"ctrl-shift-$KEYMAP_ESCAPE # Copilot disable completions"
+	"ctrl-[ # Copilot show previous completions"
+	"ctrl-] # Copilot show next completions"
+	"ctrl-{enter} # Copilot apply next word"
+	"ctrl-shift-{enter} # Copilot apply next line" # TODO use {} b/c they are not on keymap
+	''
+	"ctrl-c # Close tabs to the right"
+	"ctrl-shift-c # Close tabs to the left"
+	"ctrl-r # Rename variable in file / file in project"
+	"ctrl-shift-r # Rename file"
+	''
+	"ctrl-f # Reformat file"
+	"ctrl-g # Toggle case"
+	"ctrl-d # Delete line (Also in \`intellij_cmd_keymap\`)"
+	''
+	"ctrl-w # Add selection for next occurrence (TextMate)"
+	"ctrl-shift-w # Unselect occurrence"
+	"ctrl-v # Select all occurrences"
+ 	"ctrl-h # Clone caret above"
+	"ctrl-t # Clone caret below"
+	"ctrl-m # Multi-column selection mode"
+	''
+	"ctrl-l # Recent locations"
+	"ctrl-z # Last edit location"
+	"ctrl-shift-z # Next edit locations"
+	"ctrl-n # Next highlighted error"
+	"ctrl-shift-n # Previous highlighted error"
+	"ctrl-s # Show context actions"
 )
 
 keymap_init $INTELLIJ_CTRL_NAMESPACE $INTELLIJ_CTRL_ALIAS "${INTELLIJ_CTRL_KEYMAP[@]}"
