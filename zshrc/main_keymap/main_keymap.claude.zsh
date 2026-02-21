55ffdbb94fdfaa7b25277af868a07aad6a5f094a
Jason Zhao <jasonzhao6@gmail.com>
2026-02-20 22:20:05 -0800

    Add more claude shortcuts

--- zshrc/main_keymap/main_keymap.claude.zsh
+++ zshrc/main_keymap/main_keymap.claude.zsh
@@ -1,5 +1,10 @@
 # shellcheck disable=SC2034 # Used by `main_keymap.zsh`
 CLAUDE_CODE_KEYMAP=(
+	'! # Bash mode'
+	'& # Background task'
+	'/ # Commands'
+	'@ # File paths'
 	'ctrl-g # Interrupt and provide new instructions'
+	'ctrl-v # Paste images'
 	'shift-{tab} # Cycle permission modes'
 )
