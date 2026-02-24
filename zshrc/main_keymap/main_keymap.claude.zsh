50985fa0e09ec3b252199bce816cc792d0b4bfc2
Jason Zhao <jasonzhao6@gmail.com>
2026-02-23 21:29:00 -0800

    Update claude default keymap

--- zshrc/main_keymap/main_keymap.claude.zsh
+++ zshrc/main_keymap/main_keymap.claude.zsh
@@ -1,11 +1,11 @@
 # shellcheck disable=SC2034 # Used by `main_keymap.zsh`
 CLAUDE_CODE_KEYMAP=(
-	'! # Bash mode'
-	'& # Background task'
-	'/ # Commands'
-	'@ # File paths'
+	'! # Run bash command'
+	'& # Queue instruction in the background'
+	'/{skill}? # Invoke Claude command or skill'
+	'@ # Reference a local file or folder'
 	'ctrl-g # Interrupt and provide new instructions'
-	'ctrl-j # Newline'
-	'ctrl-v # Paste images'
+	'ctrl-j # Add newline'
+	'ctrl-v # Paste image'
 	'shift-{tab} # Cycle permission modes'
 )
