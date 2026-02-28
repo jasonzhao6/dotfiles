dc97c20f7cb51f7f16e5755de04df7df6e4f1e5c
Jason Zhao <jasonzhao6@gmail.com>
2026-02-28 09:49:37 -0800

    Update claude shortcuts

--- zshrc/main_keymap/main_keymap.claude.zsh
+++ zshrc/main_keymap/main_keymap.claude.zsh
@@ -1,10 +1,10 @@
 # shellcheck disable=SC2034 # Used by `main_keymap.zsh`
 CLAUDE_CODE_KEYMAP=(
 	'! # Run bash command'
-	'& # Queue instruction in the background'
+	'& # Run prompt in the background'
 	'/{skill}? # Invoke Claude command or skill'
 	'@ # Reference a local file or folder'
-	'ctrl-g # Interrupt and provide new instructions'
+	'ctrl-g # Compose prompt in editor'
 	'ctrl-j # Add newline'
 	'ctrl-v # Paste image'
 	'shift-{tab} # Cycle permission modes'
